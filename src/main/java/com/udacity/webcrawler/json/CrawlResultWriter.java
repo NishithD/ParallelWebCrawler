49d1c187bd0afdaba4c9a765ce8e58a7ef98d1b1
Xi Chen <x.chen0724@gmail.com>
2021-10-30 19:38:03 +0200

    Implement crawler output.

--- src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -1,5 +1,10 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -8,37 +13,47 @@ import java.util.Objects;
  * Utility class to write a {@link CrawlResult} to file.
  */
 public final class CrawlResultWriter {
-  private final CrawlResult result;
+    private final CrawlResult result;
 
-  /**
-   * Creates a new {@link CrawlResultWriter} that will write the given {@link CrawlResult}.
-   */
-  public CrawlResultWriter(CrawlResult result) {
-    this.result = Objects.requireNonNull(result);
-  }
+    /**
+     * Creates a new {@link CrawlResultWriter} that will write the given {@link CrawlResult}.
+     */
+    public CrawlResultWriter(CrawlResult result) {
+        this.result = Objects.requireNonNull(result);
+    }
 
-  /**
-   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Path}.
-   *
-   * <p>If a file already exists at the path, the existing file should not be deleted; new data
-   * should be appended to it.
-   *
-   * @param path the file path where the crawl result data should be written.
-   */
-  public void write(Path path) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(path);
-    // TODO: Fill in this method.
-  }
+    /**
+     * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Path}.
+     *
+     * <p>If a file already exists at the path, the existing file should not be deleted; new data
+     * should be appended to it.
+     *
+     * @param path the file path where the crawl result data should be written.
+     */
+    public void write(Path path) {
+        // This is here to get rid of the unused variable warning.
+        Objects.requireNonNull(path);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.valueOf(path)))) {
+            write(bufferedWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-  /**
-   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Writer}.
-   *
-   * @param writer the destination where the crawl result data should be written.
-   */
-  public void write(Writer writer) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(writer);
-    // TODO: Fill in this method.
-  }
+    /**
+     * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Writer}.
+     *
+     * @param writer the destination where the crawl result data should be written.
+     */
+    public void write(Writer writer) {
+        // This is here to get rid of the unused variable warning.
+        Objects.requireNonNull(writer);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(com.fasterxml.jackson.core.JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+        try {
+            objectMapper.writeValue(writer, result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
